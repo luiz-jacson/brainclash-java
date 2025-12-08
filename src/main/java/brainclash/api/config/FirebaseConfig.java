f7ceb497b2c4881b988114121a681ede16c4da4a
luiz982 <juninholjl@hotmail.com>
2025-12-08 10:27:26 -0300

    fix: config firebase

---
+++ src/main/java/brainclash/api/config/FirebaseConfig.java
@@ -0,0 +1,35 @@
+package brainclash.api.config;
+
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.cloud.FirestoreClient;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        InputStream serviceAccount = new ClassPathResource("static/brainclash-145c2-firebase-adminsdk-fbsvc-960454f906.json").getInputStream();
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+
+    @Bean
+    public Firestore firestore(FirebaseApp firebaseApp) {
+        return FirestoreClient.getFirestore(firebaseApp);
+    }
+}
--- src/main/java/brainclash/api/controller/CategoriaController.java
+++ src/main/java/brainclash/api/controller/CategoriaController.java
@@ -1,19 +1,42 @@
 package brainclash.api.controller;
 
 
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+
 @RestController
 @RequestMapping("/categoria")
 public class CategoriaController {
+    private final Firestore firestore;
 
-    @GetMapping
-    void getCategorias(){
+    public CategoriaController(Firestore firestore) {
+        this.firestore = firestore;
+    }
 
+    @GetMapping(value = "/getAll")
+    public String getCategorias(){
+        CollectionReference collectionReference = firestore.collection("categorias");
+        String result = "";
+        try {
 
-
+            QuerySnapshot querySnapshot = collectionReference.get().get();
+            for (QueryDocumentSnapshot queryDocumentSnapshot : querySnapshot) {
+                result += queryDocumentSnapshot.getString("nome") + " ";
+            }
+            
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
 }
