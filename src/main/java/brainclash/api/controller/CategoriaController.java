af4aa5dbd1aaa0f36316055918f9ca40a62265b0
luiz982 <juninholjl@hotmail.com>
2025-12-08 09:52:00 -0300

    firebase admin sdk

--- src/main/java/brainclash/api/controller/CategoriaController.java
+++ src/main/java/brainclash/api/controller/CategoriaController.java
@@ -1,15 +1,19 @@
 package brainclash.api.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
-public class HelloController {
+@RequestMapping("/categoria")
+public class CategoriaController {
 
     @GetMapping
-    public String olaMundo(){
-        return "Hello World!";
+    void getCategorias(){
+
+
+
     }
+
 }
